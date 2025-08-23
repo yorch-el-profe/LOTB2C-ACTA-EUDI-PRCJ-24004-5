7bca8e0d6b3cb0632d7429739fff2cecd4132a4f
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-08-23 11:11:43 -0600

    Endpoint para convertir monedas

--- sesion02-spring/src/main/java/org/bedu/demo/controller/TipoCambioController.java
+++ sesion02-spring/src/main/java/org/bedu/demo/controller/TipoCambioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.bedu.demo.model.TipoCambio;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,4 +31,33 @@ public class TipoCambioController {
     }
 
     // Cambiar la moneda en MXN
+    /**
+     *  QueryString: Enviar parámetros al final de la URL de la forma:
+     *      ? param1=valor1 & param2=valor2 & param3=valor3 ...
+     * 
+     *  Limitante: El tamaño máximo de una URL son 2000 caracteres aprox.
+     * 
+     *  El querystring es más usado para filtar búsqueda.
+     * 
+     *  Nota: Por default, los parámetros en Spring son obligatorios.
+     */
+    @RequestMapping("convertir")
+    public TipoCambio convertir(
+        @RequestParam("moneda") String moneda, 
+        @RequestParam("cantidad") int cantidad) {
+            TipoCambio buscado = null;
+
+            for (TipoCambio tipo : tipoCambios) {
+                if (tipo.getMoneda().equals(moneda)) {
+                    buscado = tipo;
+                    break;
+                }
+            }
+
+            if (buscado == null) {
+                return null;
+            }
+
+            return new TipoCambio(moneda, cantidad * buscado.getValor());
+    }
 }
