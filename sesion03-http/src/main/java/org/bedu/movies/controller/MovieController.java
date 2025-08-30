0297468da22ec01933b4dcdbb821c2112d034ed3
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-08-30 10:07:33 -0600

    Primer endpoint del API de peliculas

--- sesion03-http/src/main/java/org/bedu/movies/controller/MovieController.java
+++ sesion03-http/src/main/java/org/bedu/movies/controller/MovieController.java
@@ -1,14 +1,39 @@
 package org.bedu.movies.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bedu.movies.model.Movie;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // En un controlador que regresa datos
 // @Controller: En un controlador que puede regresar HTML o datos
 public class MovieController {
+
+    private List<Movie> db = new LinkedList<>();
+
+    public MovieController() {
+        Movie superman = new Movie();
+
+        superman.setTitle("Superman");
+        superman.setDirector("James Gunn");
+        superman.setYear(2025);
+
+        db.add(superman);
+    }
     
     // Endpoint: Acciones/operaciones que realiza el backend
+    // Endpoint = Método + anotaciones
+    // Los endpoint se acceden a través de una URL
+    // url-backend/endpoint
 
     // Obtener todas las peliculas
+    // URL: localhost:8080/getMovies
+    @RequestMapping("getMovies")
+    public List<Movie> getMovies() {
+        return db;
+    }
 
     // Obtener una sola pelicula
 
