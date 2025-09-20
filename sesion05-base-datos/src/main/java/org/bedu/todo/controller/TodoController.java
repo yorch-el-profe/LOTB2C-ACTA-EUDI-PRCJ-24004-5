d7be0951d4ab23fb871fa0d3fdfe9c01e26a188c
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-09-20 11:07:14 -0600

    Habilitando CORS#

--- sesion05-base-datos/src/main/java/org/bedu/todo/controller/TodoController.java
+++ sesion05-base-datos/src/main/java/org/bedu/todo/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.bedu.todo.entity.Todo;
 import org.bedu.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("todos")
 public class TodoController {
 
