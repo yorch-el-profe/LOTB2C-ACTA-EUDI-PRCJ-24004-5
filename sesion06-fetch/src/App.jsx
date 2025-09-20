c7286a52c11c84a6a74201e03af08f6ba0cc7e50
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-09-20 10:43:45 -0600

    Ajustes visuales

--- sesion06-fetch/src/App.jsx
+++ sesion06-fetch/src/App.jsx
@@ -10,14 +10,17 @@ function App() {
   function createTodo(event) {
     event.preventDefault();
 
-    const description = todoInput.current.value;
+    const description = todoInput.current.value.trim();
 
-    setTodos([{ description, completed: false }, ...todos]);
+    if (description) {
+      setTodos([{ description, completed: false }, ...todos]);
+    }
+
+    todoInput.current.value = "";
   }
 
   function completeTodo(index) {
     return function (event) {
-      event.preventDefault();
       const todo = todos[index];
       todo.completed = event.target.checked;
       setTodos([ ...todos ]);
@@ -25,30 +28,37 @@ function App() {
   }
 
   return (
-    <div className="d-flex flex-column">
-      <div className="input-group mb-3">
-        <input type="text" className="form-control" ref={todoInput} />
-        <button className="btn btn-outline-secondary" type="button" onClick={createTodo}>Button</button>
-      </div>
+    <div className="vh-100 vw-100 p-5">
+      <div className="container justify-content-center">
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" ref={todoInput} />
+          <button className="btn btn-primary" type="button" onClick={createTodo}>Agregar</button>
+        </div>
 
-      <div className="row">
-        {
-          todos.map((todo, index) => (
-            <div className="col-sm-6 mb-3 mb-sm-0">
-              <div className="card">
-                <div className="card-body">
-                  <p className="card-text">{todo.description}</p>
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value={todo.completed} id="checkDefault" onChange={completeTodo(index)} />
-                    <label className="form-check-label" for="checkDefault">
-                      { todo.completed ? 'Cancelar' : 'Completar'}
-                    </label>
+        <div className="row row-gap-3">
+          {
+            todos.map((todo, index) => (
+              <div className="col-sm-6 mb-3 mb-sm-0" key={index}>
+                <div className="card">
+                  <div className="card-body">
+                    <p className={ todo.completed ? "card-text text-decoration-line-through" : "card-text" }>{todo.description}</p>
+                    <div className="row justify-content-between px-2">
+                      <div className="form-check col-6">
+                        <input className="form-check-input" type="checkbox" id="checkDefault" checked={todo.completed} onChange={completeTodo(index)} />
+                        <label className="form-check-label" htmlFor="checkDefault">
+                          { todo.completed ? 'Cancelar' : 'Completar'}
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <button className="btn btn-sm btn-danger">Eliminar</button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))
-        }
+            ))
+          }
+        </div>
       </div>
     </div>
   )
