ee9501625d8c7fd27e4e2fdfada9325a2ee9d322
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-09-27 09:32:47 -0600

    Creando proyecto para la sesion 07

---
+++ sesion07-seguridad/src/main/java/org/bedu/todo/controller/TodoController.java
@@ -0,0 +1,58 @@
+package org.bedu.todo.controller;
+
+import java.util.List;
+
+import org.bedu.todo.entity.Todo;
+import org.bedu.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService service;
+    
+    @GetMapping
+    public List<Todo> findAll(
+        @RequestParam(name = "q", required = false) String q) {
+            if (q != null) {
+                // Buscar todos las tareas que tengan por nombre "q"
+                return service.filterByDescription(q);
+            } else {
+                return service.findAll();
+            }
+        
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Todo save(@RequestBody Todo todo) {
+        return service.save(todo);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable("id") int id) throws Exception {
+        service.update(id);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable("id") int id) {
+        service.deleteById(id);
+    }
+}
---
+++ sesion07-seguridad/src/main/java/org/bedu/todo/entity/Todo.java
@@ -0,0 +1,52 @@
+package org.bedu.todo.entity;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// Entidad = Tabla de base de datos
+@Entity
+// Renombrando la tabla
+@Table(name = "todos")
+public class Todo {
+    
+    @Id
+    // Va a generar su propio ID en cada inserción
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    // @Column agrega una columna a la tabla
+    // nullable hace obligatoria la columna
+    // columnDefinition permite cambiar el tipo (es especifico para cada base)
+    @Column(nullable = false)
+    private String description;
+
+    @Column
+    // @ColumnDefault agrega un valor por defecto a la columna
+    @ColumnDefault("false")
+    private boolean completed;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
